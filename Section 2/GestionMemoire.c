9374add86cb31c842e2162ce8801aeaf6524c34a
Rosemarie Millette <romila@l4714-25.info.polymtl.ca>
2024-11-13 11:32:49 -0500

    début partie 2

--- Section 2/GestionMemoire.c
+++ Section 2/GestionMemoire.c
@@ -1,31 +1,40 @@
 #include<stdbool.h>
 #include "./libs/lib.h"
+#define TAILLE_PAGE 1024
 
 unsigned int calculerNumeroDePage(unsigned long adresse) {
-	// TODO
-    return 0;
+    return adresse/TAILLE_PAGE;
 }
 
 unsigned long calculerDeplacementDansLaPage(unsigned long adresse) {
-	// TODO
-     return 0;
+     return adresse%TAILLE_PAGE;
 }
 
 unsigned long calculerAdresseComplete(unsigned int numeroDePage, unsigned long deplacementDansLaPage) {
-	// TODO
-     return 0;
+     return (numeroDePage * TAILLE_PAGE) + deplacementDansLaPage;
 }
 
 void rechercherTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	// TODO
+	if(req.estDansTLB) {
+		req.adressePhysique = calculerAdresseComplete(req.adresseVirtuelle);
+		req.date = // date de mtn
+		// mettre à jour les autres attributs?
+	} else {
+		req.adressePhysique = 0;
+	}
 }
 
 void rechercherTableDesPages(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	// TODO
+	if (calculerNumeroDePage(req.adresseVirtuelle) == mem.memoire.numeroPage) {
+		req.adressePhysique = calculerAdresseComplete(req.adresseVirtuelle);
+		// mettre à jour les autres attributs?
+	} else {
+		req.adressePhysique = 0;
+	}
 }
 
 void ajouterDansMemoire(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
-	//TODO
+	mem.tlb.numeroCadre[0] = 
 }
 
 void mettreAJourTLB(struct RequeteMemoire* req, struct SystemeMemoire* mem) {
